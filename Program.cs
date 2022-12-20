361f900a9ad919baf3f68eae8b7a194fc39f4960
mohamed-ehab2004 <114025042+mohamed-ehab2004@users.noreply.github.com>
2022-12-20 20:48:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GIVE ME FIRST NUMBER ");
+            int N1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("GIVE ME SECOND NUMBER ");
+            int N2 = int.Parse(Console.ReadLine());
+            int M;
+            int K;
+            int C;
+            Console.Write("The perfect numbers between {0} and {1} are : \n", N1, N2);
+
+
+            for (M = N1; M <= N2; M++)
+            {
+                K = 0;
+
+
+                for (C = 1; C < M; C++)
+                {
+
+                    if (M % C == 0)
+                    {
+                        K += C;
+
+
+
+                    }
+                }
+
+                if (K == M)
+
+                    Console.WriteLine(M);
+
+            }
+        }
+    }
+}
